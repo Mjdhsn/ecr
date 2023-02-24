18a3c4a9989e38936db2d8a631f5a09a322bee81
Mjdhsn <108516853+Mjdhsn@users.noreply.github.com>
2023-02-24 08:27:22 +0100

    Update lambda_function.py

--- code/lambda_function.py
+++ code/lambda_function.py
@@ -311,7 +311,7 @@ def preprcess_file(image_path):
 def lambda_handler(event, context):
     s3_client = boto3.client('s3')
     
-    bucket_name = event['Records'][0]['s3']['bcuket']['name']
+    bucket_name = event['Records'][0]['s3']['bucket']['name']
     key = event['Records'][0]['s3']['object']['key']
     key = urllib.parse.unquote_plus(key, encoding = 'utf-8')
     tmpkey = key.replace('/', '')
